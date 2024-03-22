ba8b252ac430cceefea259701c1777be7b993557
adlsdztony <zzl0712@outlook.com>
2024-03-22 11:49:13 +0800

    Update version to 2.1.2

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,3 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
